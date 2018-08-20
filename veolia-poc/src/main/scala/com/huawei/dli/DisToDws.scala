e4956e116584468d662aadd783151d3ad5b5616b
zhaoxianwei <zhaoxw_cug@126.com>
2018-08-20 12:03:31 +0800

    Add code

---
+++ veolia-poc/src/main/scala/com/huawei/dli/DisToDws.scala
@@ -0,0 +1,109 @@
+package com.huawei.dli
+
+import java.sql.{Connection, Driver, DriverManager}
+
+import com.google.gson.JsonParser
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dis.{ConsumerStrategies, DISUtils}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.slf4j.LoggerFactory
+
+case class DwsInfo(url: String, userName: String, password: String, driver: String)
+
+object DisToDws {
+  val log = LoggerFactory.getLogger("veolia")
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setAppName("Spark streaming DIS example")
+
+    log.info("Start DIS Spark Streaming demo.")
+
+    //
+    val (endpoint, region, ak, sk, projectId, streamName, startingOffsets, duration)
+    = ("https://dis.cn-north-1.myhwclouds.com:20004", "cn-north-1",
+      "9E4YAAKGMSERKEEUDXCH", "yq6jTN2HfDdOnvMRaaINF81SiCmpfReeF3a9A6PV", "769b7a1e32ea43b3ab8362070556d81c",
+      "dis-veolia", "LATEST", "10")
+
+    val (dwsurl, username, dwspassword, driver) = ("jdbc:postgresql://veolia-dws.dws.myhuaweiclouds.com:8000/postgres",
+      "dbadmin", "-rq82019", "org.postgresql.Driver")
+
+
+    val ssc = new StreamingContext(sparkConf, Seconds(duration.toInt))
+
+    val params = Map(
+      DISUtils.PROPERTY_ENDPOINT -> endpoint,
+      DISUtils.PROPERTY_REGION_ID -> region,
+      DISUtils.PROPERTY_AK -> ak,
+      DISUtils.PROPERTY_SK -> sk,
+      DISUtils.PROPERTY_PROJECT_ID -> projectId)
+    val stream = DISUtils.createDirectStream[String, String](
+      ssc,
+      ConsumerStrategies.Assign[String, String](streamName, params, startingOffsets))
+
+    val realValue = stream.map(record => record.value.toString)
+
+    realValue.foreachRDD {
+      rdd =>
+          rdd.foreachPartition(savePartitionToDws(DwsInfo(dwsurl, username, dwspassword, driver)))
+    }
+
+    //start the computation
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def savePartitionToDws(dwsInfo: DwsInfo) (iter: Iterator[String]): Unit = {
+    var connection: Connection = null
+    Class.forName(dwsInfo.driver)
+    // val property = new Property
+    try {
+      connection = DriverManager.getConnection(dwsInfo.url, dwsInfo.userName, dwsInfo.password)
+      val statement = connection.createStatement()
+      val sql = getBatchInsertSql(iter)
+      statement.execute(sql)
+      statement.close
+    } finally {
+      if (connection != null) connection.close()
+    }
+  }
+
+  def getBatchInsertSql(iter: Iterator[String]): String = {
+    val parser = new JsonParser
+    val stringBuilder = new StringBuilder
+    stringBuilder.append("insert into dbadmin.sample_tbl values ")
+    var hasRecord = false
+    while (iter.hasNext) {
+      try {
+        hasRecord = true
+        val message = iter.next()
+        val obj = parser.parse(message).getAsJsonObject
+        val equId = obj.get("equId").getAsString
+        val equType = obj.get("equType").getAsInt
+        val zoneId = obj.get("zoneId").getAsInt
+        val uploadTime = obj.get("uploadTime").getAsBigInteger
+        val runningTime = obj.get("runningTime").getAsDouble
+        val temperature = obj.get("temperature").getAsDouble
+        val pressure = obj.get("pressure").getAsDouble
+        val waterLine = obj.get("waterLine").getAsDouble
+        val targetTemperature = obj.get("targetTemperature").getAsDouble
+        val targetPressure = obj.get("targetPressure").getAsDouble
+        val targetWaterLine = obj.get("targetWaterLine").getAsDouble
+        val feedWater = obj.get("feedWater").getAsDouble
+        val noxEmissions = obj.get("noxEmissions").getAsDouble
+        val unitLoad = obj.get("unitLoad").getAsDouble
+        val fieldSeq = Seq(equId, equType, zoneId, uploadTime, runningTime, temperature, pressure, waterLine,
+          targetTemperature, targetPressure, targetWaterLine, feedWater, noxEmissions, unitLoad)
+        stringBuilder.append(fieldSeq.mkString("(", ",", "),"))
+      } catch {
+        case e: Exception =>
+          log.error("Parse message failed!", e)
+      }
+    }
+    if (hasRecord) {
+      stringBuilder.toString.stripSuffix(",") + ";"
+    } else {
+      ""
+    }
+  }
+}
---
+++ veolia-poc/src/main/scala/com/huawei/dli/DisToObs.scala
@@ -0,0 +1,79 @@
+package com.huawei.dli
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.google.gson.JsonParser
+import org.apache.hadoop.io.compress.GzipCodec
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dis.{ConsumerStrategies, DISUtils}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.slf4j.LoggerFactory
+
+object DisToObs {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val sparkConf = new SparkConf().setAppName("Spark streaming DIS example")
+    val log = LoggerFactory.getLogger("veolia")
+    log.info("Start DIS Spark Streaming demo.")
+
+    val (endpoint, region, ak, sk, projectId, streamName, startingOffsets, duration)
+        = ("https://dis.cn-north-1.myhwclouds.com:20004", "cn-north-1",
+      "9E4YAAKGMSERKEEUDXCH", "yq6jTN2HfDdOnvMRaaINF81SiCmpfReeF3a9A6PV", "769b7a1e32ea43b3ab8362070556d81c",
+      "dis-veolia2", "LATEST", "10")
+
+
+    val ssc = new StreamingContext(sparkConf, Seconds(duration.toInt))
+
+    val params = Map(
+      DISUtils.PROPERTY_ENDPOINT -> endpoint,
+      DISUtils.PROPERTY_REGION_ID -> region,
+      DISUtils.PROPERTY_AK -> ak,
+      DISUtils.PROPERTY_SK -> sk,
+      DISUtils.PROPERTY_PROJECT_ID -> projectId)
+    val stream = DISUtils.createDirectStream[String, String](
+      ssc,
+      ConsumerStrategies.Assign[String, String](streamName, params, startingOffsets))
+
+    val realValue = stream.map(record => record.value.toString)
+
+    realValue.foreachRDD {
+      rdd =>
+          val timestamp = new SimpleDateFormat("yyyy-MM-dd-hh-mm").format(new Date)
+          val timeStampArr = timestamp.split("-")
+          val year = timeStampArr(0)
+          val month = timeStampArr(1)
+          val day = timeStampArr(2)
+          val hour = timeStampArr(3)
+          val minute = timeStampArr(4)
+          rdd.map(appendTimeStamp(_, timestamp)).saveAsTextFile(
+            s"s3a://$ak:$sk@obs-veolia/test2/$year/$month/$day/$hour/$minute", classOf[GzipCodec])
+    }
+
+    //start the computation
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+
+  // timeStamp yyyy-MM-dd-hh-mm
+  private def appendTimeStamp(message: String, timeStamp: String): String = {
+    val timeStampArr = timeStamp.split("-")
+    val year = timeStampArr(0)
+    val month = timeStampArr(1)
+    val day = timeStampArr(2)
+    val hour = timeStampArr(3)
+    val minute = timeStampArr(4)
+
+    val parser = new JsonParser
+    val obj = parser.parse(message).getAsJsonObject
+    obj.addProperty("dli_year", year)
+    obj.addProperty("dli_month", month)
+    obj.addProperty("dli_day", day)
+    obj.addProperty("dli_hour", hour)
+    obj.addProperty("dli_minute", minute)
+    obj.toString
+  }
+}
